7469c3b938238cf917a369ecd04b9c37c799ff8c
Kyle Geppert <krgeppert@gmail.com>
2013-10-12 12:46:14 -0700

    Scaffolded views.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -3,5 +3,8 @@
 angular.module('catalystApp', [])
   .config(function ($routeProvider) {
     $routeProvider.when('/', { templateUrl: 'views/main.html', controller: 'MainCtrl' });
+    $routeProvider.when('/marcus', { templateUrl: 'views/main.html', controller: 'MainCtrl' });
+    $routeProvider.when('/user/tumbs', { templateUrl: 'views/main.html', controller: 'MainCtrl' });
+    $routeProvider.when('/user/question', { templateUrl: 'views/main.html', controller: 'MainCtrl' });
     $routeProvider.otherwise({ redirectTo: '/'});
   });
