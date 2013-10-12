876daafdcf03b30a52bd919e7d15752179e5ada1
Kyle Geppert <krgeppert@gmail.com>
2013-10-12 13:27:23 -0700

    Restructured app.'s. Added some stuff to marcus.

--- app/scripts/controllers/marcus.js
+++ app/scripts/controllers/marcus.js
@@ -2,9 +2,7 @@
 
 angular.module('catalystApp')
   .controller('MarcusCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+    $scope.loadQuestions(value){
+      console.log(value);
+    }
   });
