0a5151b85101505c064d2fc31e58348d91784893
Tuhin Chakraborty <tuhin@hackreactor.com>
2013-10-12 15:44:04 -0700

    Begins implementing thumbs

--- app/scripts/controllers/thumbs.js
+++ app/scripts/controllers/thumbs.js
@@ -2,9 +2,7 @@
 
 angular.module('catalystApp')
   .controller('ThumbsCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+    $scope.thumbClicked = function(data){
+    	socket.emit('thumbClicked', data);
+    };
   });
